ce0e95f71e03d0409d289155374edecf20e8eccc
bourgesl <bourges.laurent@gmail.com>
2020-02-13 14:09:29 +0100

    added arraycopy results with Xmx2G settings

--- bench_array.sh
+++ bench_array.sh
@@ -5,9 +5,20 @@ source env.sh
 # build
 mvn verify
 
+JAVA_OPTS="-Xms2g -Xmx2g -XX:+AlwaysPreTouch"
+
+echo "JAVA_OPTS: $JAVA_OPTS"
+
 # run Java benchmarks:
-#$JAVA_HOME/bin/java -jar target/PerfFX-benchmarks.jar -jvm $JAVA_HOME/bin/java
+$JAVA_HOME/bin/java $JAVA_OPTS -jar target/PerfFX-benchmarks.jar -jvm $JAVA_HOME/bin/java
 
 # run GraalVM benchmarks:
-$GRAALVM_HOME/bin/java -jar target/PerfFX-benchmarks.jar -jvm $GRAALVM_HOME/bin/java
+# CE
+export GRAALVM_HOME=/home/bourgesl/apps/graalvm-ce-java11-19.3.1/
+$GRAALVM_HOME/bin/java $JAVA_OPTS -jar target/PerfFX-benchmarks.jar -jvm $GRAALVM_HOME/bin/java
 
+# EE
+export GRAALVM_HOME=/home/bourgesl/apps/graalvm-ee-java11-19.3.1/
+$GRAALVM_HOME/bin/java $JAVA_OPTS -jar target/PerfFX-benchmarks.jar -jvm $GRAALVM_HOME/bin/java
+
+echo "done."
